0c41df4adb1637b9148e6b4d0f7d87f2dcf5d0b5
khalil123-vip <138829112+khalil123-vip@users.noreply.github.com>
2024-02-10 01:08:31 +0100

    Add files via upload

---
+++ newfile.chat.py
@@ -0,0 +1,16 @@
+#pip install openai
+import openai
+openai.api_key= "sk-ctSXCZilnS0d0Lmzdrt6T3BlbkFJKWW3 6SKWKLtR6HhTgo7"
+while True:
+ ask = input("Rakwan ali : ")
+ response= openai.Completion.create(
+    model = "text-davinci-003",
+    prompt=ask,
+    temperature=0.9,
+    max_tokens=150,
+    top_p=1,
+    frequency_penalty=0.0,
+    presence_penalty=0.6,
+) 
+text =response['choices'][0] ['text']
+print("ChatGPT :" +text)
