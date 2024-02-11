cb38d86ec5994c14f5960b11dc53845029a779de
khalil123-vip <138829112+khalil123-vip@users.noreply.github.com>
2024-02-11 06:05:45 +0100

    Update newfile.chat.py

--- newfile.chat.py
+++ newfile.chat.py
@@ -2,12 +2,12 @@ pip install openai
 import openai
 openai.api_key= "sk-ctSXCZilnS0d0Lmzdrt6T3BlbkFJKWW3 6SKWKLtR6HhTgo7"
 while True:
- ask = input("Rakwan ali : ")
+ ask = input("khalil vip : ")
  response= openai.Completion.create(
     model = "text-davinci-003",
     prompt=ask,
     temperature=0.9,
-    max_tokens=150,
+    max_tokens=600,
     top_p=1,
     frequency_penalty=0.0,
     presence_penalty=0.6,
